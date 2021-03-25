33a9f49ff5563d5eb5f7d62b80fec978ca3eab5a
Divyanshi239 <75576256+Divyanshi239@users.noreply.github.com>
2021-03-25 16:33:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,28 @@ function take_snapshot() {
 }
 
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/9mIPetO4z/model.json",modelLoaded);
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+}
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else 
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
